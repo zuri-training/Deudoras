c6675cc5173b27fed6a60414cd6264abc48b23e3
jean-on-hub <jean.eudes@amalitech.org>
2022-07-27 12:30:49 +0000

    hide secret key

--- Deudoras/Deudoras/settings.py
+++ Deudoras/Deudoras/settings.py
@@ -18,9 +18,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
+from decouple import config
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2@jxmltji)w9-y9p7-a-8)a)@8l!orj+^#i#4^wp30$9bf_36('
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
