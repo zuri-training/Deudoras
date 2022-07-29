62f9f0fd0ebc27945d652651aff992fd67f26ea6
kalu Divine <kaludivine545@gmail.com>
2022-07-29 17:18:01 +0100

    second commit

--- Deudoras/debtors/models.py
+++ Deudoras/debtors/models.py
@@ -14,3 +14,18 @@ class User(models.Model):
     email = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
 
+class Debtors(models.Model):
+    name = models.CharField(max_length=200)
+    student = models.IntegerField()
+    school_class = models.CharField(max_length=200)
+    info = models.TextField()
+    school_id = models.ForeignKey(School,on_delete=models.DO_NOTHING)
+
+class Comment(models.Model):
+    school_id = models.ForeignKey(School,on_delete=models.DO_NOTHING)
+    comment = models.TextField()
+    datetime = models.DateTimeField()
+
+class Contend(models.Model):
+    proof_of_pay = models.FileField()
+    details = models.TextField()
