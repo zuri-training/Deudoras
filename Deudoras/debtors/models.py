c557a9d208cba1e0b9197999a41ab4d2ededabc7
kalu Divine <kaludivine545@gmail.com>
2022-07-29 15:53:17 +0100

    First commit

--- Deudoras/debtors/models.py
+++ Deudoras/debtors/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    logo = models.ImageField()
+    certificate = models.FileField()
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
