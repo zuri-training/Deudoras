d5084cca23e3a695258e66e7fa0bc1d168b5ff5c
jean-on-hub <jean.eudes@amalitech.org>
2022-08-07 10:08:53 +0000

    style

--- Deudoras/Deudoras/settings.py
+++ Deudoras/Deudoras/settings.py
@@ -115,9 +115,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'Frontend'),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
