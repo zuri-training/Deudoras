5a4697850e3f0357e77b0476253c36c9f28f2898
Sobola Gabriel <sobolagabriel@gmail.com>
2022-08-08 12:34:54 +0100

    Add files via upload

--- Deudoras/Frontend/script/Regex_signup_normaluser2.js
+++ Deudoras/Frontend/script/Regex_signup_normaluser2.js
@@ -3,8 +3,6 @@ var myInput = document.getElementById("password").value;
 var myInput2 = document.getElementById("cpassword").value;
 
 
-
-
 if(myInput == ""){
     document.getElementById("message1").innerHTML = "**Fill the password please!";
     return false;
@@ -20,20 +18,19 @@ if(myInput.length < 8){
     return false;
 }
 
-
+/*
 var numbers = /[0-9]/g;
-if (!myInput.match(numbers)){
+if not (myInput.match(numbers)){
     document.getElementById("message1").innerHTML = "**Password length must contain a number";
     return false;
 }    
-  
 
-var upperCaseLetters = /[A-Z]/g;
-if (!myInput.match(upperCaseLetters)){
+var upperCaseLettters = /[A-Z]/g;
+if not (myInput.match(upperCaseLetters)){
     document.getElementById("message1").innerHTML = "**Password length must contain an uppercase";
     return false;
-}
-  
+}*/
+ 
     
 if(myInput != myInput2){
     document.getElementById("message2").innerHTML = "**Passwords are not same";
@@ -44,34 +41,21 @@ if(myInput != myInput2){
 }
 }
 
-/*
-togglePassword.addEventListener('click', function () {
-  // toggle the type attribute
-  const type =
-    password.getAttribute('type') === 'password' ? 'text' : 'password'
-  password.setAttribute('type', type)
-
-  // toggle the icon
-  this.classList.toggle('bi-eye')
-}*/
-
 
 /* check box for password */
 function myFunction(){
-  var p = document.getElementById("password");
-  if (p.type === "password"){
-    p.type = "text";
+  if (myInput.type === "password"){
+    myInput.type = "text";
   }else{
-    p.type = "password";
+    myInput.type = "password";
   }
 }
 
 /* check box for confirm password */
 function myFunc(){
-  var cp = document.getElementById("cpassword");
-  if (cp.type === "password"){
-    cp.type = "text";
+  if (myInput2.type === "password"){
+    myInput2.type = "text";
   }else{
-    cp.type = "password";
+    myInput2.type = "password";
   }
-} 
+}
