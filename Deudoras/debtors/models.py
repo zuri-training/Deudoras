ac7e69b6895165c7f7bd4221120a8422c08cdff8
jean-on-hub <jean.eudes@amalitech.org>
2022-08-08 18:13:58 +0000

    update models

--- Deudoras/debtors/models.py
+++ Deudoras/debtors/models.py
@@ -1,38 +1,56 @@
+from email.mime import image
+from pyexpat import model
 from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
 class School(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
     logo = models.ImageField()
     location = models.TextField()
     CAC = models.IntegerField()
-    certificate = models.FileField()
+    Local_government = models.CharField(max_length=200)
 
 class Debtors(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     student_id = models.IntegerField()
     student_class = models.CharField(max_length=200)
-    info = models.TextField()
+    address = models.CharField(max_length=200)
     school_id = models.ForeignKey(School,on_delete=models.DO_NOTHING)
-    amount_owed = models.DecimalField(max_digits=15,decimal_places=2)
+    contact = models.IntegerField()
 
+class Debt(models.Model):
+    id = models.AutoField(primary_key=True)
+    session = models.CharField(max_length=200)
+    stclass = models.CharField(max_length=200)
+    amount = models.DecimalField( max_digits=12, decimal_places=2)
+    term = models.IntegerField()
 class User(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
     debtors_id = models.ForeignKey(Debtors,on_delete=models.CASCADE)
+    school_id = models.ForeignKey(School, on_delete= models.CASCADE)
 
-
-class Comment(models.Model):
+class Dispute(models.Model):
     school_id = models.ForeignKey(School,on_delete=models.DO_NOTHING)
     comment = models.TextField()
     datetime = models.DateTimeField()
-    # section_id = models.ForeignKey(School,on_delete=models.CASCADE)
+    status = models.CharField(max_length=200)
+    proofofpayment = models.FileField()
+    debt_id = models.ForeignKey(Debt,on_delete=CASCADE)
+    
 
-class Contend(models.Model):
-    proof_of_pay = models.FileField()
-    details = models.TextField()
-    student_id = models.ForeignKey(Debtors,on_delete=models.CASCADE)
+class Article(models.Model):
+    id = models.AutoField(primary_key=True)
+    poster = models.ForeignKey(School, on_delete=models.CASCADE)
+    text = models.TextField()
+    date = models.DateField()
+    image = models.FileField()
+    document = models.FileField()
+    summary = models.CharField()
