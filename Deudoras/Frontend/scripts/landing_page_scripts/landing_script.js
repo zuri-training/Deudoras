150fb85778226727b3f3c7fd2d3f1eb45413a395
Eze Emmanuel Chisom <48598344+quilspace@users.noreply.github.com>
2022-08-08 17:01:46 +0100

    help centre page updated

--- Deudoras/Frontend/scripts/landing_page_scripts/landing_script.js
+++ Deudoras/Frontend/scripts/landing_page_scripts/landing_script.js
@@ -11,3 +11,47 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+/*Button on clicks */
+
+const loginButton = (document.getElementsByName("nav-button-1").onclick =
+  function () {
+    location.href = "";
+  });
+const signupButton = (document.getElementsByClassName("nav-button-2").onclick =
+  function () {
+    location.href = "";
+  });
+const seeMoreButton = (document.getElementsByClassName("see-more").onclick =
+  function () {
+    location.href = "";
+  });
+const viewCommentButton = (document.getElementsByClassName(
+  "view-comment"
+).onclick = function () {
+  location.href = "";
+});
+const signup = (document.getElementsByClassName("sign-up").onclick =
+  function () {
+    location.href = "";
+  });
+const registerAccountButton = (document.getElementsByClassName(
+  "center register-account"
+).onclick = function () {
+  location.href = "";
+});
+const privacyProtectedButton = (document.getElementsByClassName(
+  "center how-privacy-protected"
+).onclick = function () {
+  location.href = "";
+});
+const cheeckIfDebtorButton = (document.getElementsByClassName(
+  "center check-if-debtor"
+).onclick = function () {
+  location.href = "";
+});
+const readMoreButton = (document.getElementsByClassName(
+  "read-more center"
+).onclick = function () {
+  location.href = "";
+});
