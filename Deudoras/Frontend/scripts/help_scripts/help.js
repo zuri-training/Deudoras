d02d4e9af4f5908d6599d6035402c0fefe48e3f6
Eze Emmanuel Chisom <48598344+quilspace@users.noreply.github.com>
2022-08-14 13:59:12 +0100

    upds

---
+++ Deudoras/Frontend/scripts/help_scripts/help.js
@@ -0,0 +1,8 @@
+const Login = (document.getElementsByClassName("nav-button-1").onclick =
+  function () {
+    location.href = "{%url('signup1')%}";
+  });
+const Signup = (document.getElementsByClassName("nav-button-2").onclick =
+  function () {
+    location.href = "{%url('Sclogin')%}";
+  });
