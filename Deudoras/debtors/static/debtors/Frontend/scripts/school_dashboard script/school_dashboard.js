dc7310bb95ae667300f5957b02f29b1d9ceff443
unknown <bsaheed79@gmail.com>
2022-08-15 03:20:53 -0700

    completed frontend task

--- Deudoras/debtors/static/debtors/Frontend/scripts/school_dashboard script/school_dashboard.js
+++
@@ -1,9 +0,0 @@
-// selectors
-let mobileNav = document.getElementById("mobile-nav");
-let btnClose = document.getElementById("close");
-let btnOpen = document.querySelector(".mobileBtnOpen");
-
-// scripting
-
-btnOpen.addEventListener("click", () => (mobileNav.style.display = "flex"));
-btnClose.addEventListener("click", () => (mobileNav.style.display = "none"));
--- Team-39-project-my-debtors/debtors/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DebtorsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'debtors'
