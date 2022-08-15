342c138a2bfd8e507fbde796d325e0785b9b54f3
Eze Emmanuel Chisom <48598344+quilspace@users.noreply.github.com>
2022-08-15 10:05:36 +0100

    a few changes

--- Deudoras/Frontend/scripts/landing_page_scripts/landing_script.js
+++ Deudoras/Frontend/scripts/landing_page_scripts/landing_script.js
@@ -35,3 +35,24 @@ const viewCommentButton = (document.getElementsByClassName(
 const signup = (document.getElementsByClassName("sain").onclick = function () {
   location.href = "{%url('signup1')%}";
 });
+
+/**collapsible buttons javascript */
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+const collapseButton = (document.getElementsByClassName(
+  "collapse-buttons"
+).onclick = function () {
+  location.href = "{%url ('About us')%}";
+});
